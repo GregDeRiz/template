b30e186fc71cb26586d00b43c7202c014efbe7f1
GregDeRIz <plasteiggregory@gmail.com>
2025-03-22 18:33:03 +0100

    Refactored code to catch error and saved articles from a list to a json file.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+import fs from 'fs';
 import puppeteer from 'puppeteer';
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
+        headless: true,
         userDataDir: './data',
     });
     const page = await browser.newPage();
@@ -20,20 +20,21 @@ import puppeteer from 'puppeteer';
         let price = "Null";
         let link = "Null";
 
-        try {
-            title = await article.$eval('a', el => el.title);
-        } catch (error) { }
-        try {
-            link = await article.$eval('a', el => el.href);
-        } catch (error) { }
-        try {
-            price = title.match(/\d{1,3},\d{2}\s€/)[0];
-        } catch (error) { }
+        try { title = await article.$eval('a', el => el.title); }
+        catch (error) { console.log("Cannot found title from article number " + counter + ": " + error); }
+        try { link = await article.$eval('a', el => el.href); }
+        catch (error) { console.log("Cannot found link from article number " + counter + ": " + error); }
+        try { price = title.match(/\d{1,3},\d{2}\s€/)[0]; }
+        catch (error) { console.log("Cannot found price from article number " + counter + ": " + error); }
 
         title = title.substring(0, title.indexOf(',')).trim();
-        if (title !== "Null") items.push({ title, price, link });
+        if (title !== "Null" || title.length > 0) items.push({ title, price, link });
     }
 
-    console.log(items);
+    fs.writeFile('items.json', JSON.stringify(items), 'utf8', (err) => {
+        if (err) throw err;
+
+        console.log("Done! Saved articles to items.json");
+    });
     await browser.close();
 })();
