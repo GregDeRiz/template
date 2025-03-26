97528c15c05cd415d213912633b39db1c6be20e4
BuildTools <unconfigured@null.spigotmc.org>
2025-03-26 15:02:54 +0100

    Optimized the code for visibility and flexibility, extends the search for accuracy.

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 import fs from 'fs';
 import puppeteer from 'puppeteer';
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: true, userDataDir: './data' });
-    const page = await browser.newPage();
+async function storing(article, message) {
+    let option = "Null";
+    try { option = article }
+    catch (error) { console.log(message + ": " + error); }
+    return option;
+}
 
-    const userSearch = "jean femme taille 38 taille haute coupe évasée noire";
-    await page.goto('https://www.vinted.fr/catalog?search_text=' + userSearch, { waitUntil: 'networkidle2' });
-    await page.waitForSelector('.feed-grid');
-
-    const articles = await page.$$('.feed-grid > .feed-grid__item');
-    let items = [];
+async function retrieveItems(articles) {
+    const items = [];
     for (const article of articles) {
         if (items.length >= 10) break;
 
         let index = articles.indexOf(article);
-        let title = "Null";
+        let title = await storing(article.$eval('a', el => el.title), "Cannot found title from article " + index);
+        let link = await storing(article.$eval('a', el => el.href), "Cannot found link from article " + index);
         let price = "Null";
-        let link = "Null";
 
-        try { title = await article.$eval('a', el => el.title); }
-        catch (error) { console.log("Cannot found title from article " + index + ": " + error); }
-        try { link = await article.$eval('a', el => el.href); }
-        catch (error) { console.log("Cannot found link from article " + index + ": " + error); }
         try { price = title.match(/\d{1,3},\d{2}\s€/)[0]; }
         catch (error) { console.log("Cannot found price from article " + index + ": " + error); }
 
@@ -30,8 +25,10 @@ import puppeteer from 'puppeteer';
         price = price.replace(',', '.');
         if (!link.includes("member") && title !== "Null" || price !== "Null") items.push({ title, price, link });
     }
-    if (fs.existsSync('result.csv')) fs.unlinkSync('result.csv');
+    return items;
+}
 
+async function storeItemsInFile(items) {
     for (const item of items) {
         fs.appendFile(
             'result.csv',
@@ -40,6 +37,28 @@ import puppeteer from 'puppeteer';
             (err) => { if (err) throw err; }
         );
     }
+}
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: true, userDataDir: './data' });
+    const page = await browser.newPage();
+
+    const userSearch = "jean femme taille haute coupe évasée";
+    const options = {"color": 1, "size": 4};
+    await page.goto(
+        'https://www.vinted.fr/catalog?search_text=' + userSearch +
+        "&disabled_personalization=true&" +
+        "color_ids[]=" + options.color + "&" +
+        "size_ids[]=" + options.size,
+        { waitUntil: 'networkidle2' }
+    );
+    await page.waitForSelector('.feed-grid');
+
+    const articles = await page.$$('.feed-grid > .feed-grid__item');
+    const items = await retrieveItems(articles);
+    if (fs.existsSync('result.csv')) fs.unlinkSync('result.csv');
+
+    await storeItemsInFile(items);
     console.log("Done! Saved " + items.length + " articles into result.csv");
     await browser.close();
 })();
